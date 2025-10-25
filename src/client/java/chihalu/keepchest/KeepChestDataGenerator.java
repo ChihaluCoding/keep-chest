055add6a6aff78f4d8cda6afee9236a1fa9fcc38
Chihalu <m.chihiro0314@gmail.com>
2025-10-25 23:37:36 +0900

    first commit

---
+++ src/client/java/chihalu/keepchest/KeepChestDataGenerator.java
@@ -0,0 +1,11 @@
+package chihalu.keepchest;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KeepChestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
